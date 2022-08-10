4ff981be2b57d79be23fc58b49dd733a56d72f7f
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-08-10 15:24:06 +0200

    working on index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('frontend/css/home.css')}}">
     <title>Salon de Coiffure</title>
 </head>
 <body>
-    <h1>SALON DE COIFFURE</h1>
+    @include('layouts.menu')
+    <section class="home">
+        <div class="container">
+            <div class="content1">
+                <h5 class="tle">Beauty Parlour</h5>
+                <h1 class="title0">BEST HAIRCUT OF THE SEASONS</h1>
+                <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsa vel autem et dolor illum ipsum temporibus totam provident porro earum facilis, itaque voluptas nisi corporis a, enim quae? Nobis?</p>
+            </div>
+            <div class="content2">
+                <img src="{{ url('/img/12.png') }}" alt="images non disponible" class="img">
+            </div>
+        </div>
+    </section>
+    <section class="appoint">
+        <div class="form">
+            <h1 class="title1">Prennez le RendezVous</h1>
+            <form action="">
+                <input type="text" name="nom" id="">
+
+            </form>
+        </div>
+    </section>
 </body>
 </html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('frontend/css/menu.css')}}">
+</head>
+<body>
+    <!-- header section starts -->
+    <header class="header">
+        <a href="#" class="logo"><i class="fas fa-splotch"></i> Saloon</a>
+        <nav class="navbar">
+             <a href="#home">Accueil</a>
+             <a href="#services">Service</a>
+             <a href="#about">A Propos</a>
+             <a href="#features">Contact</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+   </header>
+   <!-- header section ends -->
+</body>
+</html>
+<script>
+    let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+     menu.classList.toggle('fa-times');
+     navbar.classList.toggle('active');
+}
+</script>
