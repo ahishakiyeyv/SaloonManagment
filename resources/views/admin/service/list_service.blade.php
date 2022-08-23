29ddbc5765875858b970c1f4bfee94aaa6c34bff
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-08-23 22:32:59 +0200

    service added

---
+++ resources/views/admin/service/list_service.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.navside')
+@section('content')
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Liste des Services</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+@endsection
